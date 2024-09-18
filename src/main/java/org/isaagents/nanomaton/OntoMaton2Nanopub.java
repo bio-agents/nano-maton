b8c9b3155f17948d6e6ec3416aa77148abd2eeab
johnson.chen <joy6677@qq.com>
2024-09-18 15:22:54 +0800

    Update content and rename

---
+++ src/main/java/org/isaagents/nanomaton/OntoMaton2Nanopub.java
@@ -0,0 +1,206 @@
+package org.isaagents.nanomaton;
+
+import au.com.bytecode.opencsv.CSVReader;
+import org.nanopub.MalformedNanopubException;
+import org.nanopub.Nanopub;
+import org.nanopub.NanopubImpl;
+import org.openrdf.model.Literal;
+import org.openrdf.model.Statement;
+import org.openrdf.model.URI;
+import org.openrdf.model.ValueFactory;
+import org.openrdf.model.impl.URIImpl;
+import org.openrdf.model.impl.ValueFactoryImpl;
+import org.openrdf.model.vocabulary.RDF;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by the ISATeam.
+ * User: agbeltran
+ * Date: 02/12/2013
+ * Time: 15:58
+ *
+ * @author <a href="mailto:alejandra.gonzalez.beltran@gmail.com">Alejandra Gonzalez-Beltran</a>
+ */
+public class OntoMaton2Nanopub {
+
+    private static final String HTTP = "http://";
+    private static final URI SUB_GRAPH_OF = new URIImpl("http://www.w3.org/2004/03/trix/rdfg-1/subGraphOf");
+
+    private URI headURI = null, nanopubURI = null, assertionGraphURI = null, provenanceGraphURI = null, pubInfoGraphURI = null, parentGraphURI = null;
+    private Map<String, URI> individualURImap = new HashMap<String, URI>();
+
+    private Collection<Statement> statementCollection = null;
+    private Map<String, String> namespaces = null;
+
+    public OntoMaton2Nanopub() {
+    }
+
+    /**
+     * Main method to use to generate a NanoPub from an OntoMaton template
+     *
+     * @param csvFilename
+     * @return
+     * @throws MalformedNanopubException
+     */
+    public Nanopub generateNanopub(String csvFilename) throws MalformedNanopubException, MalformedNanoMatonTemplateException {
+
+        ValueFactory factory = ValueFactoryImpl.getInstance();
+
+        statementCollection = new ArrayList<Statement>();
+        namespaces = new HashMap<String, String>();
+        namespaces.put("np", "http://www.nanopub.org/nschema#");
+
+        parseNanoMatonTemplate(csvFilename, factory);
+        Nanopub nanopub = new NanopubImpl(statementCollection, new ArrayList(namespaces.keySet()), namespaces);
+
+        return nanopub;
+    }
+
+
+    /**
+     * Reads the OntoMaton template for creating nanopublications.
+     */
+    private void parseNanoMatonTemplate(String csvFilename,
+                                        ValueFactory factory) throws MalformedNanoMatonTemplateException {
+        try {
+            CSVReader csvReader = new CSVReader(new FileReader(csvFilename));
+            Statement stmt;
+
+            String[] nextLine = null;
+            while ((nextLine = csvReader.readNext()) != null) {
+
+                stmt = null;
+
+                if (nextLine != null) {
+
+                    if (nextLine[0].startsWith((NanoMatonTemplateSyntax.NANOPUB_URI))) {
+
+                        nanopubURI = factory.createURI(nextLine[1]);
+                        headURI = factory.createURI(nextLine[1] + "head");
+
+                        stmt = factory.createStatement(nanopubURI, RDF.TYPE, Nanopub.NANOPUB_TYPE_URI, headURI);
+                        namespaces.put("this", nanopubURI.toString());
+
+                    } else if (nextLine[0].startsWith((NanoMatonTemplateSyntax.ASSERTION_GRAPH_URI))) {
+
+                        assertionGraphURI = factory.createURI(nextLine[1]);
+                        stmt = factory.createStatement(nanopubURI, Nanopub.HAS_ASSERTION_URI, assertionGraphURI, headURI);
+                        namespaces.put("assertion", assertionGraphURI.toString());
+
+                    } else if (nextLine[0].startsWith((NanoMatonTemplateSyntax.PUB_INFO_GRAPH_URI))) {
+
+                        pubInfoGraphURI = factory.createURI(nextLine[1]);
+                        stmt = factory.createStatement(nanopubURI, Nanopub.HAS_PUBINFO_URI, pubInfoGraphURI, headURI);
+                        namespaces.put("pubInfo", pubInfoGraphURI.toString());
+
+                    } else if (nextLine[0].startsWith((NanoMatonTemplateSyntax.PROVENANCE_GRAPH_URI))) {
+
+                        provenanceGraphURI = factory.createURI(nextLine[1]);
+                        stmt = factory.createStatement(nanopubURI, Nanopub.HAS_PROVENANCE_URI, provenanceGraphURI, headURI);
+                        namespaces.put("provenance", provenanceGraphURI.toString());
+
+                    } else if (nextLine[0].startsWith((NanoMatonTemplateSyntax.PARENT_GRAPH_URI))) {
+
+                        parentGraphURI = factory.createURI(nextLine[1]);
+                        stmt = factory.createStatement(nanopubURI, SUB_GRAPH_OF, parentGraphURI, headURI);
+                        namespaces.put("rdfg", "http://www.w3.org/2004/03/trix/rdfg-1/");
+
+
+                    } else if (nextLine[0].startsWith(NanoMatonTemplateSyntax.ASSERTION)) {
+
+                        stmt = parseStatement(nextLine, factory, assertionGraphURI);
+
+                    } else if (nextLine[0].startsWith(NanoMatonTemplateSyntax.PROVENANCE)) {
+
+                        stmt = parseStatement(nextLine, factory, provenanceGraphURI);
+
+                    } else if (nextLine[0].startsWith(NanoMatonTemplateSyntax.PUB_INFO)) {
+
+                        stmt = parseStatement(nextLine, factory, pubInfoGraphURI);
+
+                    } else if (nextLine[0].startsWith(NanoMatonTemplateSyntax.PREFIX)) {
+
+
+                        if (!nextLine[1].equals("") && !nextLine[2].equals(""))
+                            namespaces.put(nextLine[1], nextLine[2]);
+
+                    }
+
+                    if (stmt != null)
+                        statementCollection.add(stmt);
+
+                }
+
+            } //while
+
+        } catch (FileNotFoundException fnfex) {
+            fnfex.printStackTrace();
+        } catch (IOException ioex) {
+            ioex.printStackTrace();
+        }
+
+    }
+
+
+    /**
+     * Parse a line in the OntoMaton template as an RDF Statement
+     *
+     * @param line
+     * @param factory
+     * @param graphURI
+     * @return
+     */
+    private Statement parseStatement(String[] line, ValueFactory factory, URI graphURI) {
+
+        URI subject = null, predicate = null, object = null;
+        Literal literalObject = null;
+        Statement stmt = null;
+
+        if (line[1].startsWith(HTTP))
+            subject = factory.createURI(line[1]);
+        else {
+
+            if (line[1].equals("this"))
+                subject = factory.createURI(nanopubURI.toString());
+            else
+                subject = factory.createURI(nanopubURI.toString(), line[1]);
+            individualURImap.put(line[1], subject);
+        }
+
+        if (line[2].startsWith(HTTP))
+            predicate = factory.createURI(line[2]);
+        else
+            predicate = factory.createURI(nanopubURI.toString(), line[2]);
+
+        if (line[3].startsWith(HTTP))
+            object = factory.createURI(line[3]);
+        else {
+            //it is either an object previously defined (see list of subjects) or a literal
+            object = individualURImap.get(line[3]);
+
+            if (object == null && line[4] != null && !line[4].equals("")) {
+                literalObject = factory.createLiteral(line[3], line[4]);
+            } else {
+                object = factory.createURI(nanopubURI.toString(), line[3]);
+                individualURImap.put(line[3], object);
+            }
+        }
+
+        if (subject != null && predicate != null && object != null && graphURI != null)
+            stmt = factory.createStatement(subject, predicate, object, graphURI);
+
+        if (subject != null && predicate != null && literalObject != null && graphURI != null)
+            stmt = factory.createStatement(subject, predicate, literalObject, graphURI);
+
+        return stmt;
+    }
+
+
+}
---
+++ src/test/java/org/isaagents/OntoMaton2NanopubTest.java
@@ -0,0 +1,119 @@
+package org.isaagents;
+
+import org.isaagents.nanomaton.MalformedNanoMatonTemplateException;
+import org.isaagents.nanomaton.OntoMaton2Nanopub;
+import org.junit.Test;
+import org.nanopub.MalformedNanopubException;
+import org.nanopub.Nanopub;
+import org.nanopub.NanopubUtils;
+import org.openrdf.rio.RDFFormat;
+import org.openrdf.rio.RDFHandlerException;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+/**
+ * Created by the ISATeam.
+ * User: agbeltran
+ * Date: 09/12/2013
+ * Time: 23:11
+ *
+ * @author <a href="mailto:alejandra.gonzalez.beltran@gmail.com">Alejandra Gonzalez-Beltran</a>
+ */
+public class OntoMaton2NanopubTest {
+
+    private void generateNanopub(String extension, RDFFormat format) throws MalformedNanopubException, MalformedNanoMatonTemplateException, RDFHandlerException, FileNotFoundException, IOException {
+        for (int i = 1; i < 10; i++) {
+            String csv = "/nanopub/nanopub" + i + ".csv";
+
+            System.out.println("csv=" + csv);
+            String filepath = getClass().getResource(csv).getFile();
+            OntoMaton2Nanopub ontoMaton2Nanopub = new OntoMaton2Nanopub();
+
+            Nanopub nanopub = ontoMaton2Nanopub.generateNanopub(filepath);
+
+            System.out.println(nanopub.toString());
+
+            File file = new File("nanopub" + i + extension);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileOutputStream oFile = new FileOutputStream(file, false);
+
+            NanopubUtils.writeToStream(nanopub, oFile, format);
+        }
+    }
+
+
+    @Test
+    public void generateNanopubsTRIGTest() throws MalformedNanopubException, MalformedNanoMatonTemplateException, RDFHandlerException, FileNotFoundException, IOException {
+        generateNanopub(".trig", RDFFormat.TRIG);
+    }
+
+
+    @Test
+    public void generateNanopubsTURTLETest() throws MalformedNanopubException, MalformedNanoMatonTemplateException, RDFHandlerException, FileNotFoundException, IOException {
+        generateNanopub(".ttl", RDFFormat.TURTLE);
+    }
+
+
+    @Test
+    public void generateNanopubTest() throws MalformedNanopubException, RDFHandlerException, MalformedNanoMatonTemplateException {
+        //String csv = "/nanopub/ontomaton-nanopub.csv";
+        //String csv = "/nanopub/soapdenovo2-nanopub1.csv";
+        String csv = "/nanopub/nanopub1.csv";
+        System.out.println("csv=" + csv);
+        String filepath = getClass().getResource(csv).getFile();
+        OntoMaton2Nanopub ontoMaton2Nanopub = new OntoMaton2Nanopub();
+
+        //ValueFactory factory = ValueFactoryImpl.getInstance();
+        //URI nanopubURI = factory.createURI("http://example.org/nanopub1");
+
+        Nanopub nanopub = ontoMaton2Nanopub.generateNanopub(filepath);
+
+        System.out.println(nanopub.toString());
+
+        //NanopubUtils.writeToStream(nanopub, System.out, RDFFormat.TRIG);
+        NanopubUtils.writeToStream(nanopub, System.out, RDFFormat.TURTLE);
+
+//        if (nanopub instanceof NanopubImpl && !((NanopubImpl) nanopub).getNsPrefixes().isEmpty()) {
+//            NanopubImpl np = (NanopubImpl) nanopub;
+//
+//
+//            for (String p : np.getNsPrefixes()) {
+//                handler.handleNamespace(p, np.getNamespace(p));
+//            }
+//        }
+
+
+//        NanopubRdfHandler handler = new NanopubRdfHandler();
+//
+//        handler.startRDF();
+//        String s = nanopub.getUri().toString();
+//        if (nanopub instanceof NanopubImpl && !((NanopubImpl) nanopub).getNsPrefixes().isEmpty()) {
+//            NanopubImpl np = (NanopubImpl) nanopub;
+//            for (String p : np.getNsPrefixes()) {
+//                handler.handleNamespace(p, np.getNamespace(p));
+//            }
+//        } else {
+//            handler.handleNamespace("this", s);
+//            handler.handleNamespace("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
+//            handler.handleNamespace("rdfs", "http://www.w3.org/2000/01/rdf-schema#");
+//            handler.handleNamespace("rdfg", "http://www.w3.org/2004/03/trix/rdfg-1/");
+//            handler.handleNamespace("xsd", "http://www.w3.org/2001/XMLSchema#");
+//            handler.handleNamespace("owl", "http://www.w3.org/2002/07/owl#");
+//            handler.handleNamespace("dc", "http://purl.org/dc/terms/");
+//            handler.handleNamespace("pav", "http://swan.mindinformatics.org/ontologies/1.2/pav/");
+//            handler.handleNamespace("np", "http://www.nanopub.org/nschema#");
+//        }
+//        for (Statement st : getStatements(nanopub)) {
+//            handler.handleStatement(st);
+//        }
+//        handler.endRDF();
+
+
+    }
+}
